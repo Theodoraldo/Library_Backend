879091713f7a0898d7739304aaf74e487c805b94
Theodoraldo <theodoraldo@gmail.com>
2024-01-19 17:45:03 +0000

    Add book model to project

---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+namespace LibraryAPI.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } 
+
+    [Required]
+    public string Author { get; set; } 
+
+    [Display(Name = "Published Date")]
+    [DataType(DataType.Date)]
+    public DateTime PublishedDate { get; set; }
+
+    [Display(Name = "Available Copies")]
+    public int AvailableCopies { get; set; }
+
+    [Required]
+    public string Pages { get; set; } 
+
+    [Required]
+    public string Notes { get; set; } 
+
+    [Required]
+    public int GenreId { get; set; } 
+
+    [ForeignKey("GenreId")]
+    public Genre Genre { get; set; } 
+}
